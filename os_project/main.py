ccd03d01f4049d72058094af194310a4b92d62d1
Shalin Jain <jain.75@iitj.ac.in>
2023-10-07 02:14:53 +0530

    Update main.py

--- os_project/main.py
+++ os_project/main.py
@@ -7,6 +7,15 @@ base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 folder_path = os.path.join(base_dir, 'os_project')
 data_path = os.path.join(folder_path, 'data')
 data_file = os.path.join(data_path, 'data.txt')
+
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+
+
+if not os.path.exists(data_file):
+    with open(data_file, 'w') as f:
+        pass
+
 os.system("ps aux>{0}".format(data_file))
 
 input_file=data_file
