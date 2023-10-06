a80d3993eaee5aaddd3be0fee68cc8e3555bfaab
Shalin Jain <jain.75@iitj.ac.in>
2023-10-07 02:44:47 +0530

    Added data into database

--- os_project/main.py
+++ os_project/main.py
@@ -1,7 +1,6 @@
 import os
 import txt_to_csv as util
-import subprocess
-
+import sqlite3
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 folder_path = os.path.join(base_dir, 'os_project')
@@ -23,3 +22,20 @@ output_file=os.path.join(data_path, 'data.csv')
 
 util.convert_txt_csv(input_file,output_file)
 util.convert_txt_csv(input_file,output_file)
+
+conn = sqlite3.connect('os_project.db')
+cursor = conn.cursor()
+# cursor.execute("DROP TABLE IF EXISTS data")
+cursor.execute("CREATE TABLE IF NOT EXISTS data (user varchar(50), pid varchar(7) primary key not null, cpu varchar(7), mem varchar(10), vsz varchar(10), rss varchar(10), tty varchar(10), stat varchar(3), start text, total_time text, command text)")
+
+with open(output_file, 'r') as f:
+    next(f)
+    for line in f:
+        data = line.strip().split(',')
+        # print(data)
+        try:
+            cursor.execute("INSERT OR REPLACE INTO data VALUES (?,?,?,?,?,?,?,?,?,?,?)", data)
+        except sqlite3.Error as e:
+            print("SQLite error:", e)
+conn.commit()
+conn.close()
