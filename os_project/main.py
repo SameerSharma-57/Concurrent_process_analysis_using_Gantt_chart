59830b6ab22f2e0708faf1aaf2a555bf8fb9b669
Shalin Jain <jain.75@iitj.ac.in>
2023-10-07 01:50:23 +0530

    Update main.py

--- os_project/main.py
+++ os_project/main.py
@@ -1,8 +1,16 @@
 import os
 import txt_to_csv as util
-os.system("ps aux>/home/kali/prac/os_project/data/data.txt")
+import subprocess
 
-input_file='/home/kali/prac/os_project/data/data.txt'
-output_file='/home/kali/prac/os_project/data/data.csv'
 
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+folder_path = os.path.join(base_dir, 'os_project')
+data_path = os.path.join(folder_path, 'data')
+data_file = os.path.join(data_path, 'data.txt')
+os.system("ps aux>{0}".format(data_file))
+
+input_file=data_file
+output_file=os.path.join(data_path, 'data.csv')
+
+util.convert_txt_csv(input_file,output_file)
 util.convert_txt_csv(input_file,output_file)
